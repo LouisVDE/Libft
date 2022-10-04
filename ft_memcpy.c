569f4f3cd9444da3ea47841a2fccbae87da849e2
Louis Vanden Eede <lovanden@fu-c3-p1.s19.be>
2022-10-04 11:56:10 +0200

    void

---
+++ ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lovanden <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 09:50:39 by lovanden          #+#    #+#             */
+/*   Updated: 2022/10/04 09:56:55 by lovanden         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char		*dest_temp;
+	unsigned const char	*src_temp;
+	unsigned int		i;
+
+	if (dest == NULL && src == NULL)
+		return (NULL);
+	i = 0;
+	dest_temp = (unsigned char *)dest;
+	src_temp = (unsigned const char *)src;
+	while (i < n)
+	{
+		dest_temp[i] = src_temp[i];
+		i++;
+	}
+	return (dest_temp);
+}
