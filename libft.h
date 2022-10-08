ce5efab2e97435d9465910908c3f9ccef1679714
LouisVDE <louis.vanden.eede@icloud.com>
2022-10-08 21:44:39 +0200

    void

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lovanden <lovanden@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 13:38:45 by lovanden          #+#    #+#             */
-/*   Updated: 2022/10/08 15:04:50 by lovanden         ###   ########.fr       */
+/*   Updated: 2022/10/08 20:48:32 by lovanden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
+# include <limits.h>
 
 #endif
