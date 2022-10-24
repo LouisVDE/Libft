f49ed98fbf3906eb5a35034772835296b11f6390
lovanden <lovanden@student.s19.be>
2022-10-24 15:07:31 +0200

    void

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: lovanden <lovanden@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 09:04:50 by lovanden          #+#    #+#             */
-/*   Updated: 2022/10/24 13:31:10 by lovanden         ###   ########.fr       */
+/*   Updated: 2022/10/24 15:00:22 by lovanden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,28 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	str = (char *)malloc(sizeof(*s) + (len + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			str[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+		str[j] = '\0';
+	return (str);
 }
