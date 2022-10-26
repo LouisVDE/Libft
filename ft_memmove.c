a0ec6c2f709dc303f0f0fc5aa4a9fe56761248af
LouisVDE <louis.vanden.eede@icloud.com>
2022-10-26 19:20:19 +0200

    void

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: lovanden <lovanden@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 10:36:12 by lovanden          #+#    #+#             */
-/*   Updated: 2022/10/19 20:54:33 by lovanden         ###   ########.fr       */
+/*   Updated: 2022/10/26 19:08:59 by lovanden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,15 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t				i;
-	unsigned char		*dst_temp;
-	const unsigned char	*src_temp;
-
-	dst_temp = (unsigned char *)dst;
-	src_temp = (unsigned char *)src;
-	i = -1;
-	if (dst == NULL && src == NULL)
-		return (NULL);
-	if (src_temp < dst_temp)
-		while (len--)
-			dst_temp[len] = src_temp[len];
-	else if (dst_temp < src_temp)
-		while (++i < len)
-			dst_temp[i] = src_temp[i];
+	if (dst > src)
+	{
+		while (len > 0)
+		{
+			len--;
+			((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
+		}
+	}
+	else
+		ft_memcpy(dst, src, len);
 	return (dst);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -6,36 +6,51 @@
 /*   By: lovanden <lovanden@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 09:04:50 by lovanden          #+#    #+#             */
-/*   Updated: 2022/10/24 15:00:22 by lovanden         ###   ########.fr       */
+/*   Updated: 2022/10/25 13:59:56 by lovanden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+static char	*ft_malloc_substr(char *sub_str, size_t start,
+		size_t len, size_t str_len)
+{	
+	if (start > str_len)
+		return (ft_calloc(1, sizeof(char)));
+	if (len > (str_len - start))
+		sub_str = malloc(sizeof(*sub_str) * (str_len - start + 1));
+	else if (len <= (str_len - start))
+		sub_str = malloc(sizeof(*sub_str) * len + 1);
+	if (!sub_str)
+		return (NULL);
+	return (sub_str);
+}
+
+char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
+	char	*sub_str;
 	size_t	i;
 	size_t	j;
-	char	*str;
+	size_t	str_len;
 
-	if (!s)
-		return (NULL);
-	if (len > ft_strlen(s + start))
-		len = ft_strlen(s + start);
-	str = (char *)malloc(sizeof(*s) + (len + 1));
 	if (!str)
 		return (NULL);
 	i = 0;
 	j = 0;
-	while (s[i])
+	sub_str = NULL;
+	str_len = ft_strlen(str);
+	sub_str = ft_malloc_substr(sub_str, (size_t) start, len, str_len);
+	if (!sub_str)
+		return (NULL);
+	while (str[i] && start < str_len)
 	{
-		if (i >= start && j < len)
+		while (i == start && j < len && str[i + j])
 		{
-			str[j] = s[i];
+			sub_str[j] = str[i + j];
 			j++;
 		}
 		i++;
 	}
-		str[j] = '\0';
-	return (str);
+	sub_str[j] = '\0';
+	return (sub_str);
 }
