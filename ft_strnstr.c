399b6149460fd5078108b83453bb4590c6dea87d
LouisVDE <louis.vanden.eede@icloud.com>
2022-12-07 21:21:56 +0100

    void

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,34 +6,37 @@
 /*   By: lovanden <lovanden@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 11:30:14 by lovanden          #+#    #+#             */
-/*   Updated: 2022/10/17 11:51:53 by lovanden         ###   ########.fr       */
+/*   Updated: 2022/11/09 21:36:56 by lovanden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *s1, const char *s2, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	char	*s1_less;
+	char	*ret;
 
-	s1_less = (char *)s1;
-	i = 0;
-	if (!*s2)
-		return (s1_less);
-	while (s1_less[i] && i < len)
+	i = -1;
+	ret = (char *) haystack;
+	if (ft_strlen(needle) == 0)
+		return (ret);
+	if (len == 0)
+		return (0);
+	while (haystack[++i])
 	{
-		j = 0;
-		while (s1_less[i + j] == s2[j] && i + j < len)
+		j = -1;
+		while (needle[++j])
 		{
-			if (!s2[j])
-				return (s1_less + i);
-			j++;
+			if (haystack[i + j] == needle[j] && i + j < len)
+			{
+				if (j == ft_strlen(needle) - 1)
+					return (&ret[i]);
+			}
+			else
+				break ;
 		}
-		if (!s2[j])
-			return (s1_less + i);
-		i++;
 	}
-	return (NULL);
+	return (0);
 }
